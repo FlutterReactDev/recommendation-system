ede9b60082b9a599f30cede68f338cb77e9fc76a
Den-codern <78246895+Den-codern@users.noreply.github.com>
2023-03-29 17:57:58 +0600

    :tada: Project initialization

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+ 
+  </React.StrictMode>,
+)
