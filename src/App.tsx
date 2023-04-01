3fabf44a9560d76d6e424932af1aa0a31504803a
Den-codern <78246895+Den-codern@users.noreply.github.com>
2023-04-01 13:58:58 +0600

    :recycle: ≧◉◡◉≦

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDVx-oYxT5cAYr_lPM3tkbqNZs65GidmMo",
-  authDomain: "recomend-system.firebaseapp.com",
-  projectId: "recomend-system",
-  storageBucket: "recomend-system.appspot.com",
-  messagingSenderId: "188336269586",
-  appId: "1:188336269586:web:59edea0086120fb07e2bcb",
-  measurementId: "G-95WNQ12WJV",
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
+import useAuthInfo from "@hooks/useAuthInfo";
+import { Divider } from "@mui/material";
+import { Button } from "@mui/material";
 
 const App = () => {
-  return <div>dsdas</div>;
+  const { user, logout } = useAuthInfo();
+
+  return (
+    <div>
+      <Button onClick={logout} variant="contained">
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import "reflect-metadata";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { RouterProvider } from "react-router";
+import { router } from "@router";
+import "./index.css";
 
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    
-  </React.StrictMode>,
-)
+    <CssBaseline />
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
