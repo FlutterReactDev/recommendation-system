c51263ed0395cedf9f034b937f444ff90226f9d7
Den-codern <78246895+Den-codern@users.noreply.github.com>
2023-04-01 13:43:56 +0600

    :wrench: Add alias for app (:D)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@router": path.resolve(__dirname, "src/router"),
+      "@service": path.resolve(__dirname, "src/service"),
+      "@fb": path.resolve(__dirname, "src/firebase"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+    },
+  },
   plugins: [react()],
-})
+});
