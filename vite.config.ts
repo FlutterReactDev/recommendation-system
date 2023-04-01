c5cf6e34c3cbc9a86ae0e48319db15ec54c46f25
Den-codern <78246895+Den-codern@users.noreply.github.com>
2023-04-01 16:50:06 +0600

    :wrench: Add alias components :)

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       "@service": path.resolve(__dirname, "src/service"),
       "@fb": path.resolve(__dirname, "src/firebase"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@components": path.resolve(__dirname, "src/components"),
     },
   },
   plugins: [react()],
